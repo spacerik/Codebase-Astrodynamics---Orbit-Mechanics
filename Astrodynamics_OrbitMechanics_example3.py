280ba7c6d381525ca734a94ecadf9c9611a7e4d8
spacerik <47058749+spacerik@users.noreply.github.com>
2022-09-13 20:51:34 +0200

    Update Astrodynamics_OrbitMechanics_example3.py

--- Astrodynamics_OrbitMechanics_example3.py
+++ Astrodynamics_OrbitMechanics_example3.py
@@ -1,7 +1,11 @@
-# Code based on this post on https://stackoverflow.com/questions/29060824/is-there-a-way-to-define-a-float-array-in-python
+# Code inspired by this post on https://stackoverflow.com/questions/29060824/is-there-a-way-to-define-a-float-array-in-python
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+
+m1 = 1.988e30 # (kg) mass of central body (mass of the sun is 1.988e30 kg, mass of Earth is 5.972E24 kg)
+G = 6.67e-11 # (Nm^2kg^-2) gravitational constant
+r1 = 696340.0E3 # Radius of the sun is 696340.0 km, radius of the Earth is 6371.0 km (only used for plotting)
 
 dt = 3600 # (s) length of timestep (3600 seconds in 1 hour)
 N = 24*365 # number of timesteps (24*365 is amount of hours in 1 year)
@@ -12,11 +16,9 @@ y[0] = 0.0 # (m) initial condition of object position on y-axis
 vx = np.zeros(N+1)
 vy = np.zeros(N+1)
 vx[0] = 0.0 # (m/s) initial condition of object velocity in x-axis direction
-vy[0] = 29780 # (m/s) initial condition of object velocity in y-axis direction
+vy[0] = 29780.0 # (m/s) initial condition of object velocity in y-axis direction (Earth velocity around Sun is 29780.0 m/s)
 ax = np.zeros(N+1)
 ay = np.zeros(N+1)
-m1 = 1.988e30 # (kg) mass of central body (mass of the sun is 1.988e30 kg)
-G = 6.67e-11 # (Nm^2kg^-2) gravitational constant
 
 def a(x,y):
     r=math.sqrt(x**2+y**2) # Distance from the central body to the object
@@ -45,5 +47,8 @@ plt.scatter(x,y,label=plotlabel)
 plt.legend(loc='upper left',fontsize='small')
 plt.xlabel('X axis (m)')
 plt.ylabel('Y axis (m)')
+circle=plt.Circle((0,0),radius=r1, color='yellow') # Yellow is used for the colour of the Sun
+ax.add_patch(circle)
+plt.show()
 
 #plt.savefig('test.png')
